44f52c6a2680142acb0d829777e1e224d20d762c
Oleg <sonziza@yandex.ru>
2020-06-17 23:06:03 +0300

    Created Homework5 + task1 completed

---
+++ Homework5/Account.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Homework5
+{
+    class Account
+    {
+        string login;
+        string password;
+        //корректный логин - строка от 2 до 10 символов (только буквы латинского алфавита или цифры, при этом цифра не может быть первой)
+        readonly static Regex loginReg = new Regex(@"^[a-zA-Z][a-zA-Z0-9]{2,10}$");
+        public Account(string ulogin, string upassword)
+        {
+            login = ulogin;
+            password = upassword;
+        }
+        public static bool CheckLogin(string ulogin)
+        {
+            bool isCorrect = loginReg.IsMatch(ulogin);
+            return isCorrect;
+        }
+        public override string ToString()
+        {
+            return $"Логин: {login} \nПароль: {password}";
+        }
+        public static string HidePassword()
+        {
+            string inpt = string.Empty;
+            while (true)
+            {
+                var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                Console.Write("*");//рисуем звезду вместо нее
+                inpt += key.KeyChar; //копим в пароль символы
+            }
+            Console.WriteLine();
+            return inpt;
+        }
+    }
+
+    public partial class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Придумайте логин. Внимание, логин должен содержать цифры и латинские, а также начинаться только с буквы");
+            string userLogin = Console.ReadLine();
+            Console.WriteLine("Введите пароль");
+            string userPassword = Account.HidePassword();
+            if (Account.CheckLogin(userLogin) == true)
+            {
+                Console.WriteLine("Вход выполнен успешно!");
+                Account acc1 = new Account(userLogin, userPassword);
+                Console.WriteLine(acc1);
+            }
+            else
+            {
+                Console.WriteLine("Проверьте корректность Вашего логина!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public partial class Program
+    {
+        static string Menu()
+        {
+            Console.WriteLine("1 - Авторизация");
+            Console.WriteLine("2 - Array class");
+            Console.WriteLine("3 - Account");
+            Console.WriteLine("4 - Matrix");
+            Console.WriteLine("q - Quit");
+            string numInfo = Console.ReadLine();
+            Console.WriteLine();
+            return numInfo.ToLower();
+        }
+
+        static void Main()
+        {
+            string number;
+            do
+            {
+                Console.Clear();
+                number = Menu();
+                switch (number)
+                {
+                    case "1":
+                        Task1();
+                        break;
+                    //case "2":
+                    //    Task2();
+                    //    break;
+                    //case "3":
+                    //    Task3();
+                    //    break;
+                    //case "4":
+                    //    Task4();
+                    //    break;
+                    case "q":
+                        Console.WriteLine("Bye-bye");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong!");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+            while (number != "q");
+        }
+    }
+}
